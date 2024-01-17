79e219d31cc2a185a51c84566ca0f5f05577027c
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-01-17 21:45:39 +0100

    Update orderbook.js

--- orderbook.js
+++ orderbook.js
@@ -1,1 +1,26 @@
 
+window.onload=()=>{
+    let range= document.getElementById("range")
+    let displayValue = document.querySelector(".value")
+        let orderBtn = document.querySelector(".order__btn")
+    let buyBtn = document.querySelector(".buyBtn")
+    let sellBtn = document.querySelector(".sellBtn")
+    
+    buyBtn.addEventListener("click",()=>{
+        orderBtn.style.backgroundColor = "#00c04b"
+        orderBtn.innerHTML = "Buy"
+        
+    })
+    
+    sellBtn.addEventListener("click",()=>{
+    orderBtn.style.backgroundColor = "#ff474c"
+        orderBtn.innerHTML = "Sell"
+    })
+    
+    
+    range.addEventListener("input",()=>{
+        displayValue.innerHTML = range.value
+    })
+    
+    
+}
