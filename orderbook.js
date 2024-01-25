53a40cfd68ef328a281e5188dc7f0e628fe64eec
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-01-25 21:12:16 +0100

    Update orderbook.js

--- orderbook.js
+++ orderbook.js
@@ -1,5 +1,6 @@
 window.onload=()=>{
     let range= document.getElementById("range")
+    let inputV = document.querySelector(".inputV")
     let displayValue = document.querySelector(".value")
     let marginValue = document.querySelector(".margin")
         let orderBtn = document.querySelector(".order__btn")
@@ -29,16 +30,28 @@ console.log(formattedNumber )
     })
     
     
-    range.addEventListener("input",()=>{
-      marginValue.innerHTML = inputValue.value === "" ? 0 : range.value * inputValue.value 
-        displayValue.innerHTML = `${
-        const formattedNumber = range.value.toLocaleString('en-US', {
+    inputV.addEventListener("input",()=>{
+    const inputCal = inputValue.value === "" ? 0 : range.value * inputValue.value 
+      marginValue.innerHTML = inputCal 
+        displayValue.innerHTML =inputCal.toLocaleString('en-US', {
   style: 'currency',
   currency: 'USD',
 });
-}`
+});
+range.addEventListener("input",()=>{
+    const inputCal = inputValue.value === "" ? 0 : range.value * inputValue.value 
+      marginValue.innerHTML = inputCal 
+        displayValue.innerHTML =inputCal.toLocaleString('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+});
         
-    })
+        
+    
     
     
 }
+
+
+
